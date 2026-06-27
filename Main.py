5b07c387e6d1c3210d57472f958973bf68c93ae3
Derffffffy <arkhipovskiyivan@gmail.com>
2026-06-28 00:08:12 +0300

    Update app status message in Main.py

--- Main.py
+++ Main.py
@@ -5,8 +5,7 @@ from kivymd.uix.label import MDLabel
 class WiFiAtakApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        return MDScreen(MDLabel(text="WiFi Atak v0.1: Система готова", halign="center"))
+        return MDScreen(MDLabel(text="WiFi Atak v0.1: В рабочем состоянии", halign="center"))
 
 if __name__ == '__main__':
     WiFiAtakApp().run()
-    
