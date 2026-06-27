ea3ae2cd0f0d689e71aaab43bb187fd6af81f240
Derffffffy <arkhipovskiyivan@gmail.com>
2026-06-28 00:23:02 +0300

    Update app label text for successful build message

--- Main.py
+++ Main.py
@@ -5,7 +5,8 @@ from kivymd.uix.label import MDLabel
 class WiFiAtakApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        return MDScreen(MDLabel(text="WiFi Atak v0.1: В рабочем состоянии", halign="center"))
+        return MDScreen(MDLabel(text="WiFi Atak v0.1: Сборка успешна", halign="center"))
 
 if __name__ == '__main__':
     WiFiAtakApp().run()
+    
